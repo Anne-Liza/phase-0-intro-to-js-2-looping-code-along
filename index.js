a6399bc7ad0677a8257ace148568b450f0bd5ba6
Anne <annewangui210@gmail.com>
2024-07-11 10:17:33 +0300

    phase-0-intro-to-js-2-looping-code-along

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solutions in this file
+const name=["Guadalupe", "Ollie", "Aki"];
+console.log(name);
+
+function writeCards(name, event){
+    const thankYouMesages = [];
+    for (let i =0; i<name.length; i++ ){
+        thankYouMesages.push (`Thank you, ${name[i]}, for the wonderful surprise gift!`);
+        debugger;
+    }
+    return thankYouMesages;
+}
+writeCards(name);
+
+function countDown(number){
+    while (number>= 0){
+        console.log(number);
+        number --;
+    }
+}
+countDown(20);
+
+
+
